14e1a16ae0fe93c1cbdd14a64d8a87b0d2108860
ArtiolaDomiCaka <artioladomi@hotmail.com>
2022-01-18 21:50:07 +0100

    just start

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import LoginPage from "./components/LoginPage";
+import MainApp from "./components/MainApp";
+import MainMessagesList from "./components/MainMessagesList";
+import SideChatList from "./components/SideChatList";
+
 export default function App() {
-  return "I'm pretty empty right now..."
+  return (
+    <main>
+      <LoginPage />
+      <MainApp />
+      <MainMessagesList />
+      <SideChatList />
+    </main>
+  );
 }
